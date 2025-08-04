ba8a72389a17ebbdd06059e8f6d2dc44070de225
sangkanfaiq <sangkanfaiq@gmail.com>
2025-08-05 02:01:27 +0700

    Create HeaderTitle.tsx

---
+++ components/HeaderTitle.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+interface HeaderTitle {
+    label: string;
+    title: string
+}
+
+const HeaderTitle = ({label, title}: HeaderTitle) => {
+  return (
+    <div>
+        <h5 className='font-light text-xs color-secondary'>{label}</h5>
+        <h3 className='font-bold text-lg color-primary' style={{marginTop: 15}}>{title}</h3>
+    </div>
+  )
+}
+
+export default HeaderTitle
