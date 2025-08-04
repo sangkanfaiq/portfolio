2f09b2d28900f03b087febf437fcca42d4c52770
sangkanfaiq <sangkanfaiq@gmail.com>
2025-08-05 02:01:26 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import HeaderTitle from "@/components/HeaderTitle";
 import Introduction from "@/components/introduction";
 
 export default function Home() {
 	return (
 		<main>
 			<Introduction/>
+			<HeaderTitle label="PROJECTS" title="Case studies" />
 		</main>
 	);
 }
