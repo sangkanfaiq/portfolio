9bf91bfe320bb6070acb61af70858ea1cd2257a3
sangkanfaiq <sangkanfaiq@gmail.com>
2025-08-05 19:10:32 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,7 +72,7 @@ export default function RootLayout({
 			<body>
 				<PageTransitionProvider>
 					<Navbar />
-					{children}
+					<div className="body-side">{children}</div>
 				</PageTransitionProvider>
 			</body>
 		</html>
