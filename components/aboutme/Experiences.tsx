d92f2ebf4fea16cfbd398739fc5bbd9feca692bc
sangkanfaiq <sangkanfaiq@gmail.com>
2025-08-05 19:10:39 +0700

    Update Experiences.tsx

--- components/aboutme/Experiences.tsx
+++ components/aboutme/Experiences.tsx
@@ -1,10 +1,11 @@
-import { Flex, Typography } from "antd";
+import { Flex, Grid, Typography } from "antd";
 import React, { useEffect } from "react";
 import { EachItems } from "../EachItems";
 import { experiencesCompany } from "@/data";
 import Aos from "aos";
 
 const Experiences = () => {
+	const screens = Grid.useBreakpoint()
 	useEffect(() => {
 		Aos.init({
 			duration: 600,
@@ -13,37 +14,23 @@ const Experiences = () => {
 	}, []);
 	return (
 		<Flex data-aos="fade-up" data-aos-offset="50" data-aos-duration="1000" style={{ marginTop: 70 }} justify="center">
-			<Flex style={{ width: "100%" }}>
-				<Flex vertical gap={12} style={{ flex: 1 }}>
-					<EachItems
-						data={experiencesCompany}
-						render={(item) => (
-							<Typography.Text key={item.id} className="font-medium text-sm color-primary">
+			<Flex wrap style={{ width: "100%" }}>
+				<EachItems
+					data={experiencesCompany}
+					render={(item, index) => (
+						<Flex vertical={!screens.md} key={index} gap={screens.md ? 24 : 0} style={{ width: "100%", marginTop: screens.md ? 12 : 24 }}>
+							<Typography.Text style={{ flex: 1 }} className="font-medium text-sm color-primary">
 								{item.company}
 							</Typography.Text>
-						)}
-					/>
-				</Flex>
-				<Flex vertical gap={12} style={{ flex: 1 }}>
-					<EachItems
-						data={experiencesCompany}
-						render={(item) => (
-							<Typography.Text key={item.id} className="font-medium text-sm color-secondary">
+							<Typography.Text style={{ flex: 1 }} className="font-medium text-sm color-secondary">
 								{item.role}
 							</Typography.Text>
-						)}
-					/>
-				</Flex>
-				<Flex vertical gap={12} style={{ flex: 1 }}>
-					<EachItems
-						data={experiencesCompany}
-						render={(item) => (
-							<Typography.Text key={item.id} className="font-medium text-sm color-secondary">
+							<Typography.Text style={{ flex: 1 }} className="font-medium text-sm color-secondary">
 								{item.start} — {item.end}
 							</Typography.Text>
-						)}
-					/>
-				</Flex>
+						</Flex>
+					)}
+				/>
 			</Flex>
 		</Flex>
 	);
