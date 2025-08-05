dca0f4277463cc2e4c991eb4638d14beef1c8710
sangkanfaiq <sangkanfaiq@gmail.com>
2025-08-05 19:10:53 +0700

    Update HeaderTitle.tsx

--- components/HeaderTitle.tsx
+++ components/HeaderTitle.tsx
@@ -7,7 +7,7 @@ interface HeaderTitle {
 
 const HeaderTitle = ({label, title}: HeaderTitle) => {
   return (
-    <div>
+    <div className='header-margin'>
         <h5 className='font-light text-xs color-secondary'>{label}</h5>
         <h3 className='font-bold text-lg color-primary' style={{marginTop: 15}}>{title}</h3>
     </div>
