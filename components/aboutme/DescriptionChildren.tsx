ec757f332892256cdd5fda6e6617f83f0a1ed1ee
sangkanfaiq <sangkanfaiq@gmail.com>
2025-08-07 00:02:19 +0700

    Update DescriptionChildren.tsx

--- components/aboutme/DescriptionChildren.tsx
+++ components/aboutme/DescriptionChildren.tsx
@@ -1,6 +1,6 @@
-import { Flex, Grid, Typography } from "antd";
-import React, { useEffect } from "react";
-import AOS from "aos";
+import { Flex, Grid } from "antd";
+import React from "react";
+import { motion, Variants } from "framer-motion";
 
 interface DescriptionInterface {
 	title: string;
@@ -10,22 +10,47 @@ interface DescriptionInterface {
 
 const DescriptionChildren = ({ title, description, subdescription }: DescriptionInterface) => {
 	const screens = Grid.useBreakpoint();
-	useEffect(() => {
-		AOS.init({
-			once: true,
-		});
-	}, []);
+	const CONTAINER_VARIANT: Variants = {
+		hidden: { opacity: 0 },
+		visible: {
+			opacity: 1,
+			transition: {
+				delay: 0.2,
+				staggerChildren: 0.2,
+			},
+		},
+	};
+
+	const ITEMS_VARIANT: Variants = {
+		hidden: {
+			opacity: 0,
+			y: 20,
+		},
+		visible: {
+			opacity: 1,
+			y: 0,
+			transition: {
+				duration: 0.4,
+				ease: "easeOut",
+			},
+		},
+	};
+
 	return (
-		<Flex justify="center" align="center" vertical data-aos="fade-up" data-aos-offset="50" data-aos-duration="1000">
-			<Flex vertical align={screens.md ? "center" : "flex-start"} style={{ textAlign: screens.md ? "center" : "left" }}>
-				<Typography.Text className="font-regular text-xs color-secondary">{title}</Typography.Text>
-				<h1 className="font-bold text-med color-primary" style={{ width: screens.md ? "80%" : "100%", marginTop: 5 }}>
-					{description}
-				</h1>
-				<p className="font-medium text-reg color-secondary" style={{ width: screens.md ? "70%" : "100%", marginTop: 25 }}>
-					{subdescription}
-				</p>
-			</Flex>
+		<Flex justify="center" align="center" vertical>
+			<motion.div variants={CONTAINER_VARIANT} initial="hidden" whileInView="visible" viewport={{ once: true, amount: 0.5 }}>
+				<Flex vertical align={screens.md ? "center" : "flex-start"} style={{ textAlign: screens.md ? "center" : "left" }}>
+					<motion.p variants={ITEMS_VARIANT} className="font-regular text-xs color-secondary">
+						{title}
+					</motion.p>
+					<motion.h1 variants={ITEMS_VARIANT} className="font-bold text-med color-primary" style={{ width: screens.md ? "80%" : "100%", marginTop: 5 }}>
+						{description}
+					</motion.h1>
+					<motion.p variants={ITEMS_VARIANT} className="font-medium text-reg color-secondary" style={{ width: screens.md ? "70%" : "100%", marginTop: 25 }}>
+						{subdescription}
+					</motion.p>
+				</Flex>
+			</motion.div>
 		</Flex>
 	);
 };
